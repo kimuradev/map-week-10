039a3742dd4506d181c52456426c6da033450948
kimuradev <leandro2ki@gmail.com>
2020-01-21 06:31:03 -0300

    Add webview

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+
+export default function Main({ navigation }) {
+  const [currentRegion, setCurrentRegion] = useState(null)
+
+  useEffect(() => {
+
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        })
+      }
+    }
+
+    loadInitialPosition();
+  }, [])
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -25.5271882, longitude: -49.355163 }} >
+        <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/4400304?s=460&v=4' }} />
+
+        <Callout onPress={() => {
+          navigation.navigate('Profile', { github_username: 'kimuradev' });
+        }}>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Leandro Kimura</Text>
+            <Text style={styles.devBio}>Japonês polaco, tenista e gaiteiro por diversão. Desenvolver Front-end por opção e Mobile por devoção. IOT maker nas horas vagas.</Text>
+            <Text style={styles.devTechs}>ReactJS, React Native ,NodeJS</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView >
+  )
+}
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#fff'
+  },
+  callout: {
+    width: 260,
+  },
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  devBio: {
+    color: '#666',
+    marginTop: 5
+  },
+  devTechs: {
+    marginTop: 5
+  }
+})
